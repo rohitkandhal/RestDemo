9d4c4baf102f75735f8a62af23439771fc904403
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-10-15 20:56:28 -0400

    First working rest calls

--- RestApp/routes/products.js
+++ RestApp/routes/products.js
@@ -6,6 +6,10 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Product = require('../models/products.js');
 
+function isObjectId(n) {
+	return mongoose.Types.ObjectId.isValid(n);
+}
+
 // READ ALL - GET : return all products in database
 // URL: /products
 router.get('/', function (req, res, next){
@@ -18,9 +22,16 @@ router.get('/', function (req, res, next){
 });
 
 // Read - GET : return unique product
-// URL: /products/id
-router.get('/id', function(req, res, next) {
-	Product.findById(req.params.id, function (error, queryResult) {
+// URL: /products/:id
+router.get('/:id', function(req, res, next) {
+	var productId;
+	
+	// Check for valid product id
+	if(!!Number(req.params.id)) {
+		productId = req.params.id;
+	}
+	
+	Product.find({id: productId}, function (error, queryResult) {
 		if(error) {
 			return next(error);
 		}
@@ -31,6 +42,7 @@ router.get('/id', function(req, res, next) {
 // Create - POST - Create a new product
 // URL: /products
 router.post('/', function (req, res, next) {
+	
 	Product.create(req.body, function (error, queryResult) {
 		if(error) {
 			return next(error);
@@ -41,8 +53,8 @@ router.post('/', function (req, res, next) {
 
 // Update - PUT
 // URL: /products/id
-router.put('/id', function (req, res, next) {
-	Product.findByIdAndUpdate(req.params.id, req.body, function (error, queryResult) {
+router.put('/:id', function (req, res, next) {
+	Product.findOneAndUpdate({id: req.params.id}, req.body, function (error, queryResult) {
 		if(error) {
 			return next(error);
 		}
@@ -52,8 +64,8 @@ router.put('/id', function (req, res, next) {
 
 // Destroy - DELETE 
 // URL: /products/id
-router.delete('/id', function (req, res, next) {
-	Product.findByIdAndRemove(req.params.id, req.body, function (error, queryResult) {
+router.delete('/:id', function (req, res, next) {
+	Product.findOneAndRemove({id: req.params.id}, function (error, queryResult) {
 		if(error) {
 			return next(error);
 		}
@@ -61,5 +73,15 @@ router.delete('/id', function (req, res, next) {
 	});
 });
 
+// Destroy All (UN SAFE) -  DELETE - /products/
+router.delete('/', function (req, res, next) {
+	// Remove all documents from product collection
+	Product.remove({}, function(err, result) {
+		if(err) {
+			return res.json('Delete error');
+		}
+		res.json(result);
+	});
+});
 
 module.exports = router;
