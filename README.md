9d4978f2f1c106a8436fc43f370a0d16fa256f0c
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2015-10-15 21:58:05 -0400

    Removed not used packages

---
+++ README.md
@@ -0,0 +1,15 @@
+###Rest API demo using nodejs
+
+A simple REST API to perform CRUD operation on a simple object
+
+###Object structure
+Product:
+	id (_integer_): Unique product identifier
+	name (_string_): Product name
+	price (_number_): Product price
+	
+###CRUD Operations
+ 1. Create 
+ 2. Read
+ 3. Update
+ 4. Delete 
--- RestApp/app.js
+++ RestApp/app.js
@@ -1,7 +1,6 @@
 // Default middlewares for express framework
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -18,7 +17,6 @@ mongoose.connect('mongodb://localhost/restApp', function(error) {
 
 // app routes configuration
 var routes = require('./routes/index');
-// var users = require('./routes/users');
 var products = require('./routes/products');
 
 var app = express();
@@ -27,14 +25,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
 app.use('/products', products);
 
 // catch 404 and forward to error handler
